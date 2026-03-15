62d59a18331b0ac7c90f176a19ec80ac430aaa59
Saitejaguptha <saitejguptha111@gmail.com>
2026-03-15 23:26:56 +0530

    Deployment fix

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { FiMail, FiCalendar, FiUser, FiLock, FiArrowLeft, FiCheckCircle } from 'react-icons/fi';
+import { FiMail, FiUser, FiLock, FiArrowLeft, FiCheckCircle } from 'react-icons/fi';
 import { toast } from 'react-hot-toast';
 
 import { addNotification } from '../utils/watchlistUtils';
